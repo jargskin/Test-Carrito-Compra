af73e8816142da652b902e5de2378bd3ac31de7f
Jordan rodriguez <j.rodriguez@smaas.org>
2018-08-29 18:18:21 -0300

    cambios finales

--- carrito-compra/src/App.vue
+++ carrito-compra/src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+   <h1>Listado de Productos</h1>
+    <div class="container global">
+      <div class="row">
+          <div id="indicadores">
+            <ul>
+              <li v-for="info in infos" :info="info" :key="info"></li>
+            </ul>
+            
+          </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="prod">
+            <producto v-for="producto in productos" :producto="producto" :key="producto.id"></producto>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="carrito">
+            <carrito-compra></carrito-compra>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import producto from './components/producto.vue'
+import CarritoCompra from './components/carrito.vue'
+import axios from 'axios'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  components:{producto, CarritoCompra},
+  data () {
+    return {
+      infos: [],
+      productos: [
+        { id:1, nombre:'cuadro uno', precio: 50, img:'producto.jpg' },
+         { id:2, nombre:'cuadro dos', precio: 20, img:'producto.jpg' },
+         { id:3, nombre:'cuadro tres', precio: 15, img:'producto.jpg' },
+         { id:4, nombre:'cuadro cuatro', precio: 25, img:'producto.jpg' }
+      ],
+      
+    }
+  },
+  mounted () {
+    axios
+      .get('https://mindicador.cl/api')
+      .then(response => (this.infos = response))
+      console.log( this.infos );
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: #eee;
+}
+.global {
+  display:flex;
+  justify-content: space-between
+}
+.prod {
+  display:flex;
+  justify-content: space-between;
+  flex-wrap:wrap
+}
+.carrito {
+  background-color:#EEEEEE;
 }
 </style>
